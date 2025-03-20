5b3846bace7243be4bd9c2e1c196089f1eb6c5c0
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-03-20 10:47:02 -0300

    gerando variaveis do dashboard

---
+++ challenge-next/src/app/dashboard/page.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { routes } from "@/routes";
+import Head from "next/head";
+import Header from "@/components/Header/Header";
+import MobileMenu from "@/components/MobileMenu/MobileMenu";
+
+const DashboardPage: React.FC = () => {
+  const router = useRouter();
+
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [tipoFalha, setTipoFalha] = useState<string>("Mecânica");
+  const [falhas, setFalhas] = useState<Falha[]>([]);
+  const [filteredFalhas, setFilteredFalhas] = useState<any[]>([]);
+  const [appliedFilter, setAppliedFilter] = useState<boolean>(false);
+  const [tipoFiltro, setTipoFiltro] = useState<string>("Todos");
+  const [showAddModal, setShowAddModal] = useState<boolean>(false);
+
+  // Estados para o modal de detalhes com atualização de status
+  const [selectedFalha, setSelectedFalha] = useState<Falha | null>(null);
+  const [showDetailsModal, setShowDetailsModal] = useState<boolean>(false);
+  const [modalStatus, setModalStatus] = useState<string>("Pendente");
