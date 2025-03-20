bbadf2a659e2c563fceddf513c6a5b923bb5e810
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-03-20 10:49:12 -0300

    funcao pegar classe

--- challenge-next/src/app/dashboard/page.tsx
+++ challenge-next/src/app/dashboard/page.tsx
@@ -140,3 +140,16 @@ const aplicarFiltro = (falhasParaFiltrar?: any[]) => {
     const temFiltroAplicado = startDate || endDate || tipo !== "Todos";
     setAppliedFilter(!!temFiltroAplicado);
   };
+
+  const getTipoClass = (tipo: string) => {
+    switch (tipo) {
+      case "Mecânica":
+        return "bg-blue-100 text-blue-800";
+      case "Elétrica":
+        return "bg-yellow-100 text-yellow-800";
+      case "Estrutural":
+        return "bg-green-100 text-green-800";
+      default:
+        return "bg-gray-100 text-gray-800";
+    }
+  };
